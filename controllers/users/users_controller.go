4d4078c8592a152f3512624dd183a001d4456f1a
Ahmet Uzgor <ahmet.uzgor@getir.com>
2021-10-22 23:25:50 +0300

    user struct and createUser function is implemented

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,28 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/ahmet-uzgor/users-api-bookstore/domain/users"
+	"github.com/ahmet-uzgor/users-api-bookstore/services"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "not implemented yet")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	result, createUserErr := services.CreateUser(user)
+	if createUserErr != nil {
+		fmt.Println(createUserErr.Error())
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {}
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,7 @@
 package services
+
+import "github.com/ahmet-uzgor/users-api-bookstore/domain/users"
+
+func CreateUser(user users.User) (*users.User, error) {
+	return &user, nil
+}
